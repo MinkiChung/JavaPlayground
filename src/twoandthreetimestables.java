318e47cbf035f70fe8464a15ee511595ccbdb5bc
정민기 <jmk05099@ggmail.com>
2021-08-29 15:21:33 +0900

    commit

---
+++ src/twoandthreetimestables.java
@@ -0,0 +1,15 @@
+public class twoandthreetimestables {
+    public static void main(String args[]) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("2 x " + i + " = " + 2 * i );
+        }
+
+        System.out.println();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println("3 x " + i + " = " + 3 * i );
+        }
+
+
+    }
+}
